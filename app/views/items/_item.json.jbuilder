4ebc17c3684641acf476d44a4a1bd1b0e333e22e
orangeman7201 <aki.jazz1027@gmail.com>
2022-11-06 17:39:16 +0900

    rails new

---
+++ app/views/items/_item.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! item, :id, :text, :text, :created_at, :updated_at
+json.url item_url(item, format: :json)
---
+++ db/migrate/20221106083829_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :text
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
