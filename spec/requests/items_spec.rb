100b2605e1bd1cc7cfa6d2deba0de3dacc7b6eb9
orangeman7201 <aki.jazz1027@gmail.com>
2022-11-06 19:53:56 +0900

    nameとpriceが保存できること

--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -2,17 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Items", type: :request do
   let!(:user) { create(:user) }
-  let!(:item) { create(:item) }
 
-  describe "GET /index" do
+  describe "/index" do
     it "returns http success" do
       get items_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /new" do
+  describe "/new" do
     subject { get new_item_path }
+    context 'ログインしていないとき' do
+      it "returns http success" do
+        expect(subject).to redirect_to new_user_session_path
+      end
+    end
     context 'ログインしているとき' do
       it "returns http success" do
         sign_in user
@@ -20,10 +24,23 @@ RSpec.describe "Items", type: :request do
         expect(response).to have_http_status(:success)
       end
     end
+  end
+
+  describe "/create" do
+    subject { post items_path, params: { name: "price", price: 200, text: "test"} }
 
     context 'ログインしていないとき' do
       it "returns http success" do
         expect(subject).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'ログインしているとき' do
+      it "itemが保存できること" do
+        sign_in user
+        expect {subject}.to change { Item.all.size }.from(0).to(1)
+        expect(Item.last.name).to eq "price"
+        expect(Item.last.price).to eq 200
       end
     end
   end
