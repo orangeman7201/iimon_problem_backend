ade509d92b2c3a9a076461c9087deb537e4e3bd3
orangeman7201 <aki.jazz1027@gmail.com>
2022-11-06 18:17:48 +0900

    不要なファイを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  root "items#index"
   devise_for :users
   resources :items
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
